a3a28eb509c653417234f966e3be8b9468d86b2a
Sherwatt <79425724+Sherwatt@users.noreply.github.com>
2022-04-18 15:16:35 -0700

    Update Rocket.js

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -19,6 +19,8 @@ class Rocket extends Phaser.GameObjects.Sprite {
                 this.x -= this.moveSpeed;
             } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width){
                 this.x += this.moveSpeed;
+            } else if (pointer.worldX <= game.config.width - borderUISize - this.width && pointer.worldX >= borderUISize + this.width) {
+                this.x = pointer.worldX  //works but do I need to slow it down?
             }
         }
         //fire button, 2nd part ensures sound won't play when rocket is in motion
